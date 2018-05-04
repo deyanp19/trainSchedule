4662484ac1769096615eef68cd60a4b3bb17b22f
deyan petrov <deyanp19@hotmail.com>
2018-05-04 17:40:29 -0500

    Bootstrap Ready

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAMZjbvJ92EbgB09oa8zj_FsYL0Va5LTvo",
+    authDomain: "train-schedule-535f1.firebaseapp.com",
+    databaseURL: "https://train-schedule-535f1.firebaseio.com",
+    projectId: "train-schedule-535f1",
+    storageBucket: "",
+    messagingSenderId: "693311659805"
+  };
+  firebase.initializeApp(config);
+
+  //Initial Values
